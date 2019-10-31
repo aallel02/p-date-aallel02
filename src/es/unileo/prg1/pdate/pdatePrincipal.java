5a48ba35dbd6b63f079f2171fefe19068deb161e
practicas <you@example.com>
2019-10-31 11:44:27 +0100

    Initial

---
+++ src/es/unileo/prg1/pdate/pdatePrincipal.java
@@ -0,0 +1,15 @@
+package es.unileo.prg1.pdate;
+
+public class pdatePrincipal {
+	public void main() {
+	
+		// TODO Auto-generated method stub
+		
+		Date miFecha= new Date();
+		Date miFecha2=new Date(1,11,2019);
+		System.out.println(miFecha);
+		System.out.println(miFecha2);
+	
+	
+	}
+}
