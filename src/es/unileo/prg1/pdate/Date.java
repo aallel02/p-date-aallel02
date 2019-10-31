294bb56854ddc11debaf7e8e182d3e32ae1b3a00
practicas <you@example.com>
2019-10-31 12:53:13 +0100

    Modificacion 1

--- src/es/unileo/prg1/pdate/Date.java
+++ src/es/unileo/prg1/pdate/Date.java
@@ -1,5 +1,10 @@
 package es.unileo.prg1.pdate;
-
+/**
+ * Implementacion de la clase Date para Programacion1
+ * 
+ * @author practicas
+ *@version 0.1
+ */
 public class Date {
 	
 		private int day;
@@ -17,7 +22,48 @@ public class Date {
 				this.month= month;
 				this.year= year;
 		}
-
+		
+		
+		
+		public boolean isSameYear(Date miFecha) {
+			boolean retorno=false;
+			if(this.year ==  miFecha.year)
+				retorno=true;
+			return retorno;
+		}
+		
+		public boolean isSameYearsinif(Date miFecha) {
+			return (this.year ==  miFecha.year);
+		}
+		
+		
+		
+		public boolean isSameMonth(Date miFecha) {
+			boolean retorno2=false;
+			if(this.month ==  miFecha.month)
+				retorno2=true;
+			return retorno2;
+		}
+		
+		public boolean isSameMonthsinif(Date miFecha) {
+			return (this.month ==  miFecha.month);
+		}
+		
+		
+		
+		public boolean isSameDay(Date miFecha) {
+			boolean retorno3=false;
+			if(this.day ==  miFecha.day)
+				retorno3=true;
+			return retorno3;
+		}
+		
+		public boolean isSameDaysinif(Date miFecha) {
+			return (this.day ==  miFecha.day);
+		}
+		
+		
+		
 		@Override
 		public String toString() {
 			return "Date [day=" + day + ", month=" + month + ", year=" + year + "]";
--- src/es/unileo/prg1/pdate/pdatePrincipal.java
+++ src/es/unileo/prg1/pdate/pdatePrincipal.java
@@ -1,7 +1,12 @@
 package es.unileo.prg1.pdate;
-
+/**
+ * Implementacion de la clase Date para Programacion1
+ * 
+ * @author practicas
+ *@version 0.1
+ */
 public class pdatePrincipal {
-	public void main() {
+	public static void main(String[] args) {
 	
 		// TODO Auto-generated method stub
 		
@@ -10,6 +15,42 @@ public class pdatePrincipal {
 		System.out.println(miFecha);
 		System.out.println(miFecha2);
 	
-	
+		
+		
+		System.out.println("Opcion 1");
+		
+		if(miFecha.isSameYear(miFecha2)) 
+			System.out.println("Es el mismo AÑO");
+		else 
+				System.out.println("Es distinto AÑO");
+		
+		if(miFecha.isSameMonth(miFecha2)) 
+			System.out.println("Es el mismo MES");
+		else
+				System.out.println("Es distinto MES");
+		
+		if(miFecha.isSameDay(miFecha2)) 
+			System.out.println("Es el mismo DIA");
+		else
+				System.out.println("Es distinto DIA");
+		
+		
+		
+		System.out.println("Opcion sin IF");
+		
+		if(miFecha.isSameYearsinif(miFecha2)) 
+			System.out.println(" Es el mismo AÑO");
+		else 
+				System.out.println("Es distinto AÑO");
+		
+		if(miFecha.isSameMonthsinif(miFecha2)) 
+			System.out.println(" Es el mismo MES");
+		else 
+				System.out.println("Es distinto MES");
+		
+		if(miFecha.isSameDaysinif(miFecha2)) 
+			System.out.println("Es el mismo DIA");
+		else 
+				System.out.println("Es distinto DIA");
 	}
 }
